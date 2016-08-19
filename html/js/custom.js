b12602e82aa78da0d8ff6c21adb6853dad0b7888
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-08-19 15:44:47 +0600

    start

---
+++ html/js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$(document).on('click', '.collapse-detector', function(e){
+		$(this).siblings('.collapse-block').slideToggle(300);
+		$(this).toggleClass('active')
+		e.preventDefault();
+	});
+
+	$(document).on('click', '.show-all-button.form-side-left', function(e){
+		$(this).toggleClass('active').siblings('.custom-hidden').slideToggle(300)
+
+		if($(this).hasClass('active')) {
+			$(this).text('Скрыть')
+		} else {
+			$(this).text('Показать все')
+		}
+
+		e.preventDefault();
+	});
+
+	if($('.selectpicker').length > 0) {
+		$('.selectpicker').selectpicker({
+			style: "selectbox",
+		  size: 15
+		});
+	}
+
+	$(document).on('click', '.product__tab[data-tab-id]', function(e){
+		tab = $('.product__tabs-item[data-tab-id="'+$(this).attr('data-tab-id')+'"]');
+
+		tab.siblings('.product__tabs-item').hide().end().show();
+
+		$(this).siblings().removeClass('active').end().addClass('active')
+		e.preventDefault();
+	});
+});
