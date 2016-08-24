cd31ac959f2dc9da48678fd538f9bcc58f8deb9b
Yuriy Bogatyrenko <yurabogatyrenko@gmail.com>
2016-08-24 18:07:19 +0600

    popups

--- html/js/custom.js
+++ html/js/custom.js
@@ -33,6 +33,13 @@ $(document).ready(function(){
 		e.preventDefault();
 	});
 
+	$(window).resize(function(){
+		if(!$('.catalog-categories__title').is(':visible')) {
+			$('.catalog-categories__listing').removeAttr('style');
+			$('.catalog-categories__title').removeClass('active');
+		}
+	})
+
 	$(document).on('click', '.catalog-categories__title', function(e){
 		if($(this).hasClass('active')) {
 			$(this).removeClass('active');
@@ -53,4 +60,9 @@ $(document).ready(function(){
 		$('.overlay').fadeIn(300);
 		pop.fadeIn(300);
 	})
+
+	$(document).on('click', '.top-line__menu-icon', function(e){
+		$(this).toggleClass('active');
+		$('.top-line__menu-listing').toggleClass('active');
+	});
 });
